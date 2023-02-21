cc41ce7cf1606899b7f17e0bb570d15381e7a8e4
JagpreetRandio <115323708+JagpreetRandio@users.noreply.github.com>
2023-02-20 17:41:51 -0800

    markdown is finished

---
+++ Develop/README.md
@@ -0,0 +1,31 @@
+# Test - Project
+  
+  ## Description 
+  Testing readme
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## Installation 
+  1. test 2. test 3. test
+
+  ## Usage 
+  instructions will be here for usage
+
+  ## License 
+  This project is covered under the MIT license.
+
+  ## Contributing 
+  Jagpreet Randio
+
+  ## Tests
+  Test instructions will be heree
+
+  ## Questions
+  If you have any questions about this projects, please contact me directly at jagpreet@test.com. You can view more of my projects at https://github.com/JagpreetRandio.
+
+  ## License
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -22,11 +22,52 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license) {
+    return '';
+  } else if (license) {
+    for (i = 0; i < 1; i++) {
+      if (license == 'MIT') {
+        return `
+        https://opensource.org/licenses/MIT
+        `
+      } else if (license == 'General Public License 2.0') {
+        return `
+        https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
+        `
+      } else if (license == 'General Public License 3.0') {
+        return `
+        https://www.gnu.org/licenses/gpl-3.0
+        `
+      } else if (license == 'Mozilla Public License 2.0') {
+        return `
+        https://opensource.org/licenses/MPL-2.0
+        `
+      } else if (license == 'BSD 3-Clause License') {
+        return `
+        https://opensource.org/licenses/BSD-3-Clause
+        `
+      } else if (license == 'Apache License 2.0') {
+        return `
+        https://opensource.org/licenses/Apache-2.0
+        `
+      }
+    }
+  }
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return '';
+  } else if (license) {
+    for (i = 0; i < 1; i++) {
+    return `## License`
+
+}
+  }
+};
 
 // TODO: Create a function to generate markdown for README
 const generateMarkdown = data => {
@@ -49,16 +90,18 @@ const generateMarkdown = data => {
   ${data.usage}
 
   ## License 
-  This project is license under ${data.license}
+  This project is covered under the ${data.license} license.
 
   ## Contributing 
   ${data.contribution}
 
   ## Tests
   ${data.test}
-  
+
   ## Questions
   If you have any questions about this projects, please contact me directly at ${data.emailAddress}. You can view more of my projects at https://github.com/${data.githubUsername}.
+
+  ${renderLicenseSection(data.license)}
 `;
 }
 
